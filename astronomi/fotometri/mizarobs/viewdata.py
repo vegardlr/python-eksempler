3dd86be09f44531585c2737fc0924d8c1d4b1984
Vegard Rekaa <vegard@rekaa.no>
2022-05-18 16:01:54 +0200

    Edit mizarobs/viewdata

--- astronomi/fotometri/mizarobs/viewdata.py
+++ astronomi/fotometri/mizarobs/viewdata.py
@@ -4,13 +4,16 @@ import numpy as np
 from astropy.time import Time
 import matplotlib.pyplot as plt
 
-#Åpne mappe og finn datafil, les til minnet. 
-if len(sys.argv) < 2:
-    print("Usage: viewdata.py datafolder")
-    sys.exit(1)
-datafolder = sys.argv[1]
-datafile = datafolder+"/data.txt"
-f = open(datafile,'r')
+#Angi hvilken mappe du vil lese data fra
+# (ta bort "#" fra den linjen du vil bruke, 
+#  og legg til en "#" fordan de du er ferdig med) 
+datafolder = "W-lyr"
+#datafolder = "IR-gem"
+#datafolder = "S-vul"
+#datafolder = "U-gem"
+
+#Les data fra mappenavn/data.txt
+f = open(datafolder+"/data.txt",'r')
 data = np.genfromtxt(f,delimiter='\t')
 
 #Lagre relevante kolonner fra datafilen som tid og mag
@@ -18,7 +21,7 @@ tid = Time(data[:,0],format='jd').to_value('datetime64')
 mag = data[:,1]
 
 #Vis data - Resten av jobben gjøres i plottevinduet eller "på ark"
-plt.plot(tid,mag,'-.')
+plt.plot(tid,mag,'.')
 plt.grid(True)
 plt.xlabel("Tid")
 plt.ylabel("Mag")
@@ -26,3 +29,4 @@ plt.title(datafolder)
 ax = plt.gca()
 ax.invert_yaxis()
 plt.show()
+
